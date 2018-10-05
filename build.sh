c542283bb1c5641e949d280df9838faef693b7ce
Ubuntu <ubuntu@ip-172-31-46-163.us-west-2.compute.internal>
2018-10-05 06:03:50 +0000

    use broadwell to build dpdk so Platinum can support

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 source ~/plink-marcopolo/initenv.sh
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $dir/server; make;
-cd $dir/client; make;
+cd $dir/server; make clean; make;
+cd $dir/client; make clean; make;
