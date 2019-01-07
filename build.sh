8620a7654097e5d55cd8e6a4fe43f79dea832622
ubuntu <ubuntu>
2019-01-07 06:38:26 +0000

    build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 source ~/plink-marcopolo/initenv.sh
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export LIBRARY_PATH=$LD_LIBRARY_PATH
+export LIBRARY_PATH=/home/ubuntu/softiwarp-user-for-linux-rdma/build/lib:/usr/local/cuda/lib64:/usr/local/lib
 cd $dir/server; make clean; make;
 cd $dir/client; make clean; make;
