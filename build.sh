d09d2e379b2e312f7471ad4fd4b1c0d012674068
ubuntu <ubuntu>
2019-01-07 06:33:03 +0000

    +

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 source ~/plink-marcopolo/initenv.sh
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export LIBRARY_PATH=$LD_LIBRARY_PATH
 cd $dir/server; make clean; make;
 cd $dir/client; make clean; make;
